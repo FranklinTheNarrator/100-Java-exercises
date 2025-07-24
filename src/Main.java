e9dc3e6e9f55e154783ced395425371ce8912333
Franklin The Narrator <daloipoffranklin@gmail.com>
2025-07-24 14:40:15 +0300

    Completed Collections module

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import collections.ListIntersection;
+
 public class Main {
     public static void main(String[] args) {
+        ListIntersection listIntersection = new ListIntersection();
 
+        listIntersection.findIntersection();
     }
 }
