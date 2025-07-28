f976870502325aeac6de0ae126e440f80c59a9de
Franklin The Narrator <daloipoffranklin@gmail.com>
2025-07-28 15:22:51 +0300

    Updated master-branch

---
+++ src/miscellaneous/CircleArea.java
@@ -0,0 +1,7 @@
+package miscellaneous;
+
+public class CircleArea {
+    public double calculateArea(int rad) {
+        return Math.PI * (Math.pow(rad, 2));
+    }
+}
---
+++ src/miscellaneous/RandomNumGenerator.java
@@ -0,0 +1,7 @@
+package miscellaneous;
+
+public class RandomNumGenerator {
+    public void generateNum() {
+        System.out.println((int) (Math.random() * 100 + 1));
+    }
+}
